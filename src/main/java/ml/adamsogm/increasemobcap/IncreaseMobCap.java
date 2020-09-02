b3f0f299b0c9fabb969dcfd874061a473b593d52
Gabriel Adamson <adamson.g@gmail.com>
2020-09-02 10:38:36 -0500

    Ensured forge won't error if mod missing on server or client, updated to 1.0.1

--- src/main/java/ml/adamsogm/increasemobcap/IncreaseMobCap.java
+++ src/main/java/ml/adamsogm/increasemobcap/IncreaseMobCap.java
@@ -1,6 +1,7 @@
 package ml.adamsogm.increasemobcap;
 
 import net.minecraft.entity.EntityClassification;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
@@ -8,6 +9,8 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -22,6 +25,9 @@ public class IncreaseMobCap {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.CONFIG);
 		Config.loadConfig(Config.CONFIG, FMLPaths.CONFIGDIR.get().resolve("increasemobcap.toml"));
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		
+		//Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 	
 	private void setup(final FMLCommonSetupEvent event)
